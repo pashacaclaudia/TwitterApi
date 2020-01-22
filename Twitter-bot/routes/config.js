0660bf9fe2b8ef6af012a45f569a2f983d87fdd8
pashacaclaudia <45997748+pashacaclaudia@users.noreply.github.com>
2020-01-23 00:56:49 +0100

    Update config.js

--- Twitter-bot/routes/config.js
+++ Twitter-bot/routes/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: 'dgi447mv76E6sHOic4FIFXXYv',  
-  consumer_secret: 'pA2d1fuAZBfp3qnyQh9sk1YBjyOlQEAGxgWlL6VIFGnY2lAzVs',
-  access_token: '1215593802196488193-z05AQRut4Nc0suPGPWjBSvyVZ19WBQ',  
-  access_token_secret: 'NiyXF2MjQzFDXZWzrDupiKTIKUkp0PK7U5YRorj6KHVHJ'
+  consumer_key: '',  
+  consumer_secret: '',
+  access_token: '',  
+  access_token_secret: ''
 }
